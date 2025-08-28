2b3bd189839931eb8724322a5bf1950174a12886
marc <mlassort@acfi-network.com>
2025-08-28 11:22:22 +0200

    Commit all files SKK.

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -80,6 +80,27 @@ class RouteController extends AbstractController
         ]);
     }
 
+    #[Route("/arts-martiaux/aikido", name: "aikido")]
+    public function aikido(): Response { return $this->render("arts-martiaux/aikido.html.twig"); }
+
+    #[Route("/arts-martiaux/judo-jujutsu", name: "judo_jujutsu")]
+    public function judoJujutsu(): Response { return $this->render("arts-martiaux/judo-jujutsu.html.twig"); }
+
+    #[Route("/arts-martiaux/karate-do", name: "karate_do")]
+    public function karateDo(): Response { return $this->render("arts-martiaux/karate-do.html.twig"); }
+
+    #[Route("/arts-martiaux/kung-fu", name: "kung_fu")]
+    public function kungFu(): Response { return $this->render("arts-martiaux/kung-fu.html.twig"); }
+
+    #[Route("/arts-martiaux/shinkyokai", name: "shinkyokai")]
+    public function shinkyokai(): Response { return $this->render("arts-martiaux/shinkyokai.html.twig"); }
+
+    #[Route("/arts-martiaux/taikibudo", name: "taikibudo")]
+    public function taikibudo(): Response { return $this->render("arts-martiaux/taikibudo.html.twig"); }
+
+    #[Route("/arts-martiaux/yoga", name: "yoga")]
+    public function yoga(): Response { return $this->render("arts-martiaux/yoga.html.twig"); }
+
     #[Route("/arts-culturels", name: "app_arts_culturels_route")]
     public function getArtsCulturelsRoute(): Response
     {
@@ -105,7 +126,7 @@ class RouteController extends AbstractController
             "total" => $total,
             "activity" => $activity,
             "artCulturel" => $artsCulturels,
-            "stripe_public_key" => $this->getParameter('stripe_public_key')
+            "stripe_public_key" => $this->getParameter("stripe_public_key")
         ]);
     }
 
@@ -139,7 +160,7 @@ class RouteController extends AbstractController
         return $this->render("arts-culturels/sumi-e.html.twig", [
             "items" => $items,
             "total" => $total,
-            'stripe_public_key' => $this->getParameter('stripe_public_key'),
+            "stripe_public_key" => $this->getParameter("stripe_public_key"),
             "ateliersSumie" => $ateliersSumie,
             "ateliersKintsugi" => $ateliersKintsugi,
             "ateliersGyotaku" => $ateliersGyotaku
@@ -162,7 +183,7 @@ class RouteController extends AbstractController
         return $this->render("arts-culturels/origami.html.twig", [
             "items" => $items,
             "total" => $total,
-            'stripe_public_key' => $this->getParameter('stripe_public_key'),
+            "stripe_public_key" => $this->getParameter("stripe_public_key"),
             "ateliersOrigami" => $ateliersOrigami
         ]);
     }
@@ -183,7 +204,7 @@ class RouteController extends AbstractController
         return $this->render("arts-culturels/calligraphie.html.twig", [
             "items" => $items,
             "total" => $total,
-            'stripe_public_key' => $this->getParameter('stripe_public_key'),
+            "stripe_public_key" => $this->getParameter("stripe_public_key"),
             "ateliersCalligraphie" => $ateliersCalligraphie
         ]);
     }
@@ -248,7 +269,7 @@ class RouteController extends AbstractController
                 "products" => $products,
                 "items" => $items,
                 "total" => $total,
-                'stripe_public_key' => $this->getParameter('stripe_public_key')
+                "stripe_public_key" => $this->getParameter("stripe_public_key")
             ]
         );
     }
@@ -270,7 +291,7 @@ class RouteController extends AbstractController
                 "products" => $products,
                 "items" => $items,
                 "total" => $total,
-                'stripe_public_key' => $this->getParameter('stripe_public_key')
+                "stripe_public_key" => $this->getParameter("stripe_public_key")
             ]
         );
     }
@@ -290,39 +311,39 @@ class RouteController extends AbstractController
         return $this->render("home/panier.html.twig", [
             "items" => $items,
             "total" => $total,
-            'stripe_public_key' => $this->getParameter('stripe_public_key')
+            "stripe_public_key" => $this->getParameter("stripe_public_key")
         ]);
     }
 
-    #[Route('/cart/add/{id}', name: 'cart_add')]
+    #[Route("/cart/add/{id}", name: "cart_add")]
     public function add(SessionInterface $session, Product $product): Response
     {
         $this->cartService->addToCart($session, $product);
-        return $this->redirectToRoute('app_panier');
+        return $this->redirectToRoute("app_panier");
     }
 
-    #[Route('/add-order', name: 'add_order_to_cart', methods: ['POST', 'GET'])]
+    #[Route("/add-order", name: "add_order_to_cart", methods: ["POST", "GET"])]
     public function addOrderToCart(
         SessionInterface $session,
         EntityManagerInterface $entityManager,
         ProductRepository $productRepository
     ): JsonResponse {
         // Récupérer les informations du panier depuis la session
-        $cart = $session->get('cart', []);
+        $cart = $session->get("cart", []);
 
         // Calculer le total du panier (vous pouvez adapter ce calcul en fonction de votre logique)
         $totalAmount = 0;
 
         // Créer la commande
         $order = new Order();
-        $order->setOrderType('panier');
-        $order->setStatus('en-cours');
+        $order->setOrderType("panier");
+        $order->setStatus("en-cours");
         $order->setCreatedAt(new DateTimeImmutable("now"));
 
         foreach ($cart as $item) {
-            $product = $productRepository->find($item['product']->getId());
+            $product = $productRepository->find($item["product"]->getId());
             $order->addProduct($product);
-            $totalAmount += $item['product']->getPrice() * $item['quantity'];
+            $totalAmount += $item["product"]->getPrice() * $item["quantity"];
         }
 
         $order->setTotalAmount($totalAmount);
@@ -332,13 +353,13 @@ class RouteController extends AbstractController
         $entityManager->flush();
 
         // Enregistrer l'ID de la commande dans la session
-        $session->set('orderId', $order->getId());
+        $session->set("orderId", $order->getId());
 
         // Retourner une réponse JSON pour confirmer la création de la commande
-        return new JsonResponse(['success' => true, 'orderId' => $order->getId()]);
+        return new JsonResponse(["success" => true, "orderId" => $order->getId()]);
     }
 
-    #[Route('/save-order-and-session', name: 'save_order_and_session', methods: ['POST', 'GET'])]
+    #[Route("/save-order-and-session", name: "save_order_and_session", methods: ["POST", "GET"])]
     public function saveOrderAndSession(
         Request $request,
         SessionInterface $session,
@@ -347,22 +368,22 @@ class RouteController extends AbstractController
     {
         $validator = Validation::createValidator();
 
-        $logoFile = $request->files->get('logo');
-        $csvFile = $request->files->get('csv');
+        $logoFile = $request->files->get("logo");
+        $csvFile = $request->files->get("csv");
 
         // Validation du logo, uniquement des fichiers image
         $logoConstraints = new Assert\File([
-            'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif'],
-            'mimeTypesMessage' => 'Veuillez télécharger une image valide (jpeg, png ou gif).',
+            "mimeTypes" => ["image/jpeg", "image/png", "image/gif"],
+            "mimeTypesMessage" => "Veuillez télécharger une image valide (jpeg, png ou gif).",
         ]);
 
         $csvConstraints = new Assert\File([
-            'mimeTypes' => [
-                'text/csv',
-                'application/vnd.ms-excel',
-                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            "mimeTypes" => [
+                "text/csv",
+                "application/vnd.ms-excel",
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             ],
-            'mimeTypesMessage' => 'Veuillez télécharger un fichier CSV ou Excel valide.',
+            "mimeTypesMessage" => "Veuillez télécharger un fichier CSV ou Excel valide.",
         ]);
 
         $logoErrors = $validator->validate($logoFile, $logoConstraints);
@@ -370,77 +391,77 @@ class RouteController extends AbstractController
 
         if (count($logoErrors) > 0 || count($csvErrors) > 0) {
             return new JsonResponse([
-                'success' => false,
-                'errors' => [
-                    'logo' => (string) $logoErrors,
-                    'csv' => (string) $csvErrors,
+                "success" => false,
+                "errors" => [
+                    "logo" => (string) $logoErrors,
+                    "csv" => (string) $csvErrors,
                 ],
             ]);
         }
 
-        $clubName = $request->get('club_name');
-        $logoFile = $request->files->get('logo');
-        $csvFile = $request->files->get('csv');
-        $email = $request->get('email');
-        $presidentName = $request->get('president_name');
-        $treasurerName = $request->get('treasurer_name');
-        $address = $request->get('address');
-        $address2 = $request->get('address2');
-        $zip = $request->get('zip');
-        $city = $request->get('city');
-        $country = $request->get('country');
+        $clubName = $request->get("club_name");
+        $logoFile = $request->files->get("logo");
+        $csvFile = $request->files->get("csv");
+        $email = $request->get("email");
+        $presidentName = $request->get("president_name");
+        $treasurerName = $request->get("treasurer_name");
+        $address = $request->get("address");
+        $address2 = $request->get("address2");
+        $zip = $request->get("zip");
+        $city = $request->get("city");
+        $country = $request->get("country");
 
         // Enregistrement du logo si présent
         if ($logoFile) {
-            $logoFilename = uniqid() . '.' . $logoFile->guessExtension();
-            $logoFile->move($this->getParameter('upload_directory'), $logoFilename);
+            $logoFilename = uniqid() . "." . $logoFile->guessExtension();
+            $logoFile->move($this->getParameter("upload_directory"), $logoFilename);
         } else {
             $logoFilename = null;
         }
 
         // Enregistrement du fichier CSV/Excel
         if ($csvFile) {
-            $csvFilename = uniqid() . '.' . $csvFile->guessExtension();
-            $csvFile->move($this->getParameter('upload_directory'), $csvFilename);
-            $csvFilePath = $this->getParameter('upload_directory') . '/' . $csvFilename;
+            $csvFilename = uniqid() . "." . $csvFile->guessExtension();
+            $csvFile->move($this->getParameter("upload_directory"), $csvFilename);
+            $csvFilePath = $this->getParameter("upload_directory") . "/" . $csvFilename;
         } else {
-            return new JsonResponse(['error' => 'No CSV or Excel file uploaded'], 400);
+            return new JsonResponse(["error" => "No CSV or Excel file uploaded"], 400);
         }
 
-        $memberCount = (int) $request->get('member_count');
+        $memberCount = (int) $request->get("member_count");
 
-        $cart['club'] = [
-            'club_name' => $clubName,
-            'logo' => $logoFilename,
-            'email' => $email,
-            'president_name' => $presidentName,
-            'treasurer_name' => $treasurerName,
-            'address' => $address,
-            'address2' => $address2,
-            'zip' => $zip,
-            'city' => $city,
-            'country' => $country,
-            'members' => $memberCount,
-            'csvFilePath' => $csvFilePath
+        $cart["club"] = [
+            "club_name" => $clubName,
+            "logo" => $logoFilename,
+            "email" => $email,
+            "president_name" => $presidentName,
+            "treasurer_name" => $treasurerName,
+            "address" => $address,
+            "address2" => $address2,
+            "zip" => $zip,
+            "city" => $city,
+            "country" => $country,
+            "members" => $memberCount,
+            "csvFilePath" => $csvFilePath
         ];
 
-        $session->set('cart', $cart);
+        $session->set("cart", $cart);
 
         $order = new Order();
-        $order->setOrderType('inscription-club');
+        $order->setOrderType("inscription-club");
         $order->setTotalAmount(50 + ($memberCount * 5));
-        $order->setStatus('en-cours');
+        $order->setStatus("en-cours");
         $order->setCreatedAt(new DateTimeImmutable());
 
         $entityManager->persist($order);
         $entityManager->flush();
 
-        $session->set('orderId', $order->getId());
+        $session->set("orderId", $order->getId());
 
-        return new JsonResponse(['success' => true]);
+        return new JsonResponse(["success" => true]);
     }
 
-    #[Route('/add-cultural-arts', name: 'add_cultural_arts_to_cart', methods: ['POST'])]
+    #[Route("/add-cultural-arts", name: "add_cultural_arts_to_cart", methods: ["POST"])]
     public function addCulturalArtsToCart(
         Request $request,
         SessionInterface $session,
@@ -449,41 +470,41 @@ class RouteController extends AbstractController
     {
         $data = json_decode($request->getContent(), true);
 
-        $price = $data['price'];
-        $registrationData = $data['registrationData'];
-        $activity = $registrationData['activity'];
+        $price = $data["price"];
+        $registrationData = $data["registrationData"];
+        $activity = $registrationData["activity"];
 
-        $session->set('cart', []);
+        $session->set("cart", []);
 
-        $cart = $session->get('cart', []);
-        $cart['cultural_registration'] = [
-            'price' => $price,
-            'activity' => $activity,
-            'first_name' => $registrationData['firstName'],
-            'last_name' => $registrationData['lastName'],
-            'birth_date' => $registrationData['birthDate'],
-            'sex' => $registrationData['sex'],
-            'email' => $registrationData['email'],
-            'type' => $registrationData['type'],
+        $cart = $session->get("cart", []);
+        $cart["cultural_registration"] = [
+            "price" => $price,
+            "activity" => $activity,
+            "first_name" => $registrationData["firstName"],
+            "last_name" => $registrationData["lastName"],
+            "birth_date" => $registrationData["birthDate"],
+            "sex" => $registrationData["sex"],
+            "email" => $registrationData["email"],
+            "type" => $registrationData["type"],
         ];
 
-        $session->set('cart', $cart);
+        $session->set("cart", $cart);
 
         $order = new Order();
         $order->setOrderType($activity);
         $order->setTotalAmount($price);
-        $order->setStatus('en-cours');
+        $order->setStatus("en-cours");
         $order->setCreatedAt(new DateTimeImmutable("now"));
 
         $entityManager->persist($order);
         $entityManager->flush();
 
-        $session->set('orderId', $order->getId());
+        $session->set("orderId", $order->getId());
 
-        return new JsonResponse(['success' => true]);
+        return new JsonResponse(["success" => true]);
     }
 
-    #[Route('/add-individual-registration', name: 'add_individual_registration_to_cart', methods: ['POST'])]
+    #[Route("/add-individual-registration", name: "add_individual_registration_to_cart", methods: ["POST"])]
     public function addIndividualRegistrationToCart(
         Request $request,
         SessionInterface $session,
